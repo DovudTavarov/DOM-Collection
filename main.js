c66c32b746bcdac2adf819da13d58dcd608dc774
DovudTavarov <dovudtavarov54@gmail.com>
2024-08-12 19:36:13 -0400

    Pass a Message

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ document.getElementById("img").addEventListener("click", function () {
       "https://learn.seytech.co/assets/projects/js/dom-projects/light-switch/lightoff.png";
   }
 });
+
+/* Pass a Message */
+
+const onSubmit = () => {
+  const val = document.querySelector("#text").value;
+  if (val.length > 0) {
+    document.querySelector("#result").innerHTML = val.toUpperCase();
+    document.querySelector("#text").value = "";
+  }
+};
+
+document.querySelector("#text").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    onSubmit();
+  }
+});
+
+/* Word Count */
