797711fdef955a33fdf177df25ddc8240a31affd
DovudTavarov <dovudtavarov54@gmail.com>
2024-08-13 19:42:26 -0400

    Tip Calculator

--- main.js
+++ main.js
@@ -31,3 +31,22 @@ document.querySelector("#text").addEventListener("keypress", function (event) {
 });
 
 /* Word Count */
+
+const calc = () => {
+  const val = document.querySelector("#count-text").value;
+  const word = val.split(" ");
+  document.querySelector("#count-result").innerHTML = word.length;
+};
+
+/* Tip Calculator */
+const submit = () => {
+  const bill = Number(document.querySelector("#amount").value);
+  const tip = Number(document.querySelector("#tip").value);
+  const people = Number(document.querySelector("#people").value);
+  let total = (bill * tip) / people;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+  document.querySelector(
+    "#totalTip"
+  ).innerHTML = `<h5>TIP AMOUNT</h5><div>${total}$</div><h5>each</h5>`;
+};
